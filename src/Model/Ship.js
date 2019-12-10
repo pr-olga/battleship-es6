de9389fa3e566f4465001cb6e4023f581f62db41
pr-olga <frkhorkovaolga@gmail.com>
2019-12-10 15:21:23 +0100

    feat: create primitive method for positioning of ships

--- src/Model/Ship.js
+++ src/Model/Ship.js
@@ -5,9 +5,23 @@ export default class Ship {
     this.name = 'Ship'
     this.amount = 0
     this.size = 0
+    this.position = []
+  }
+
+  positionShip () {
+    const posBegin = Math.floor(Math.random() * 10) + 1
+    let posEnd = posBegin + this.size
+
+    if (posEnd > 10) {
+      posEnd = posBegin - this.size
+    }
+
+    this.position = [posBegin, posEnd]
+
+    return this.position
   }
 
   createShip () {
-    console.log(`A new Ship(s) is(are) on the water: ${this.name}, amount: ${this.amount}, numberCols: ${this.size}`)
+    console.log(`A new Ship(s) is(are) on the water: ${this.name}, amount: ${this.amount}, numberCols: ${this.size}, position: ${this.positionShip()}`)
   }
 }
