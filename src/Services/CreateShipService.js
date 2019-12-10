8c6f03d54ed335555c23856f280b72424380353f
pr-olga <frkhorkovaolga@gmail.com>
2019-12-10 22:23:18 +0100

    refractor: add CreateShipService

---
+++ src/Services/CreateShipService.js
@@ -0,0 +1,46 @@
+'use strict'
+
+import ShipFactory from '../Model/Ship/ShipFactory'
+import Field from '../Model/Field/Field'
+
+export default class CreateShipService {
+  constructor () {
+    this.flotilla = this._instantiateShips()
+    this.positions = this._generatePositions()
+  }
+
+  positionShips () {
+    const flotilla = this.flotilla
+    const positions = this.positions
+
+    // assign begin and end positions
+    for (let i = 0; i <= flotilla.length; i++) {
+      for (const position in flotilla[i]) {
+        flotilla[i].position = positions[i]
+        flotilla[i].position.end[0] = positions[i].begin[0] - flotilla[i].size
+
+        if (flotilla[i].position.end[0] < 0) {
+          flotilla[i].position.end[0] = positions[i].begin[0] + flotilla[i].size
+        }
+
+        flotilla[i].position.end[1] = positions[i].begin[1]
+      }
+    }
+
+    return flotilla
+  }
+
+  _generatePositions () {
+    return new Field().generatePositions()
+  }
+
+  _instantiateShips () {
+    const factory = new ShipFactory()
+    const battleship = factory.createShips('Battleship')
+    const cruiser = factory.createShips('Cruiser')
+    const destroyer = factory.createShips('Destroyer')
+    const submarine = factory.createShips('Submarine')
+
+    return [battleship, cruiser, destroyer, submarine]
+  }
+}
--- src/app.js
+++ src/app.js
@@ -1,33 +1,9 @@
-import ShipFactory from './Model/Ship/ShipFactory'
-import Field from './Model/Field/Field'
+import PlaceShips from './Services/CreateShipService'
 
 require('./assets/sass/style.scss')
 
-const factory = new ShipFactory()
-const battleship = factory.createShips('Battleship')
-const cruiser = factory.createShips('Cruiser')
-const destroyer = factory.createShips('Destroyer')
-const submarine = factory.createShips('Submarine')
-
-const flotilla = [battleship, cruiser, destroyer, submarine]
-const positions = new Field().generatePositions()
-
-// assign begin and end positions
-for (let i = 0; i <= flotilla.length; i++) {
-  for (const position in flotilla[i]) {
-    flotilla[i].position = positions[i]
-    flotilla[i].position.end[0] = positions[i].begin[0] - flotilla[i].size
-
-    if (flotilla[i].position.end[0] < 0) {
-      flotilla[i].position.end[0] = positions[i].begin[0] + flotilla[i].size
-    }
-
-    flotilla[i].position.end[1] = positions[i].begin[1]
-  }
-}
-
-const setGame = [battleship, cruiser, destroyer, submarine]
-console.log(setGame)
+const setGame = new PlaceShips()
+console.log(setGame.positionShips())
 console.log('Ships moved to their positions!')
 
 setGame ? console.log('-> Everything is prepeared! Fiiiiiire!!!') : console.log('Something went wrong')
