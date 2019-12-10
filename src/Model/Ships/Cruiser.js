2418987bd60aa081d8ce50e9772969b260dfda8e
pr-olga <frkhorkovaolga@gmail.com>
2019-12-10 14:49:58 +0100

    refractor: put ship parameter into subclasses constructor

--- src/Model/Ships/Cruiser.js
+++ src/Model/Ships/Cruiser.js
@@ -5,8 +5,8 @@ import Ship from '../Ship'
 export default class Cruiser extends Ship {
   constructor () {
     super()
-    this.name = 'Cruiser',
-    this.amount = 2,
+    this.name = 'Cruiser'
+    this.amount = 2
     this.size = 3
 
     super.createShip()
